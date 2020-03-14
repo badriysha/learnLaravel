7affa44fe55ffc32fb2a6591014537077bf89a4c
Badri Yusron <badriysha@Badris-MacBook-Pro.local>
2020-03-14 23:30:19 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout/main')
+
+@section('title', 'Arabiya Indonesia')
+
+@section('container')
+<div class="container">
+    <h1>Hello, Admin</h1>
+</div>
+@endsection
---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layout/main')
+
+@section('title', 'Daftar Mahasiswa')
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-3">Daftar Mahasiwa</h1>
+
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">NRP</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Jurusan</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach($mahasiswa as $mhs)
+                    <tr>
+                        <th scope="col">{{ $loop->iteration }}</th>
+                        <td>{{ $mhs->name }}</td>
+                        <td>{{ $mhs->nrp }}</td>
+                        <td>{{ $mhs->email }}</td>
+                        <td>{{ $mhs->jurusan }}</td>
+                        <td>
+                            <a href="" class="badge badge-success">edit</a>
+                            <a href="" class="badge badge-danger">hapus</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
