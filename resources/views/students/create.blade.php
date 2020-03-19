59c657272a95b5915bb7f366fe79d1bef99c2257
Badri Yusron <badriysha@Badris-MacBook-Pro.local>
2020-03-20 00:11:06 +0700

    Add old input in form stdents

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -11,25 +11,25 @@
                 @csrf
                 <div class=" form-group">
                     <label for="nama">Nama</label>
-                    <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" name="nama">
+                    <input type="text" class="form-control @error('nama') is-invalid @enderror" id="nama" name="nama" value="{{ old('nama') }}">
                     @error('nama')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="nrp">NRP</label>
-                    <input type="text" class="form-control @error('nrp') is-invalid @enderror" id="nrp" name="nrp">
+                    <input type="text" class="form-control @error('nrp') is-invalid @enderror" id="nrp" name="nrp" value="{{ old('nrp') }}">
                     @error('nrp')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="form-group">
                     <label for="email">Email</label>
-                    <input type="text" class="form-control" id="email" name="email">
+                    <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}">
                 </div>
                 <div class="form-group">
                     <label for="jurusan">Jurusan</label>
-                    <input type="text" class="form-control" id="jurusan" name="jurusan">
+                    <input type="text" class="form-control" id="jurusan" name="jurusan" value="{{ old('jurusan') }}">
                 </div>
 
                 <button type="submit" class="btn btn-primary">Tambah data</button>
